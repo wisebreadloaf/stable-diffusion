3d4e9417f4817cde62864ecea8ab32825d15ea3b
wisebreadloaf <kpprasanna24@gmail.com>
2024-02-21 14:18:15 +0530

    final diffusion

--- sd/decoder.py
+++ sd/decoder.py
@@ -27,6 +27,7 @@ class VAE_ResidualBlock(nn.Module):
         super().__init__()
         self.groupnorm_1 = nn.GroupNorm(32, in_channels)
         self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
         self.conv_2 = nn.GroupNorm(32, out_channels, kernel_size=3, padding=1)
 
         if in_channels == out_channels:
